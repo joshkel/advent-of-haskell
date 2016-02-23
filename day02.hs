fe34eec36ea6c577f907bf75c891edf1a5436810
Josh Kelley <joshkel@gmail.com>
2016-02-22 23:07:03 -0500

    Day 2 phase 2

--- day02.hs
+++ day02.hs
@@ -1,10 +1,19 @@
 import Data.List (sort)
 import Data.List.Split (splitOn)
 
-wrapping_paper_area s =
-    let [a,b,c] = sort [read x :: Int | x <- splitOn "x" s]
-    in 3*a*b + 2*a*c + 2*b*c
+tuplify3 :: [a] -> (a,a,a)
+tuplify3 [x,y,z] = (x,y,z)
+
+box_dimensions :: String -> (Int,Int,Int)
+box_dimensions s = tuplify3 (sort [read x :: Int | x <- splitOn "x" s])
+
+wrapping_paper_area :: (Int,Int,Int) -> Int
+wrapping_paper_area (a,b,c) = 3*a*b + 2*a*c + 2*b*c
+
+ribbon_length :: (Int,Int,Int) -> Int
+ribbon_length (a,b,c) = 2*a + 2*b + a*b*c
 
 main = do
     contents <- getContents
-    print (sum(map wrapping_paper_area (lines contents)))
+    putStrLn ("Wrapping paper: " ++ show (sum(map (wrapping_paper_area . box_dimensions) (lines contents))))
+    putStrLn ("Ribbon:         " ++ show (sum(map (ribbon_length . box_dimensions) (lines contents))))
