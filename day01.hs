75a487ab4cd6c608ef4b11ecb92922eaa7033dbc
Josh Kelley <joshkel@gmail.com>
2016-02-22 22:15:16 -0500

    Add day 1 step 2

--- day01.hs
+++ day01.hs
@@ -7,7 +7,11 @@ elevator (x:xs) = dir x + elevator xs
           dir ')' = -1
           dir x = 0
 
+stop_at_floor :: String -> Int -> Int
+stop_at_floor s stop_at = head [ n | n <- [1..length s], let e = elevator (take n s), e == stop_at ]
+
 main = do
     [f] <- getArgs
     s <- readFile f
-    putStrLn (show (elevator s))
+    putStrLn ("Final position: " ++ (show (elevator s)))
+    putStrLn ("Reach floor -1: " ++ (show (stop_at_floor s (-1))))
