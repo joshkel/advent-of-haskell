727c3f294d7e5d2466bddfdb0f439833a3d05601
Josh Kelley <joshkel@gmail.com>
2016-03-09 22:22:37 -0500

    Day 4 part 2.  Not yet working.

--- day04.hs
+++ day04.hs
@@ -4,7 +4,10 @@ key = "bgvyzdsv"
 
 hash n = md5s $ Str (key ++ show n)
 
-advent_coins = [ i | i <- [1..], take 5 (hash i) == "00000" ]
+advent_coins prefix = [ i | i <- [1..], take 5 (hash i) == prefix ]
+
+zeroes n = replicate n '0'
 
 main = do
-    putStrLn (show $ head advent_coins)
+    putStrLn ("Five leading zeroes " ++ show (head $ advent_coins (zeroes 5)))
+    putStrLn ("Six leading zeroes " ++ show (head $ advent_coins (zeroes 6)))
